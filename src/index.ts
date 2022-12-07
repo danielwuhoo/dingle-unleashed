61f5c0db6322ea459180b2d5687a18bdb3e92eba
danielwuhoo <danielwuhoo@gmail.com>
2022-12-06 20:45:28 -0500

    add express

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,19 @@ import 'regenerator-runtime/runtime';
 import 'reflect-metadata';
 import { container } from 'tsyringe';
 import dotenv from 'dotenv';
+import express from 'express';
 import Client from './clients/DingleClient';
 
 dotenv.config();
 
 const client: Client = container.resolve(Client);
+const app = express();
 client.init();
+
+app.get('/', (req, res) => {
+    res.send('Hello from Express server!');
+});
+
+app.listen(3000, () => {
+    console.log('Express server listening on port 3000');
+});
