9efb4ab0f919f9e49e4414849fef0abfd27a3933
danielwuhoo <danielwuhoo@gmail.com>
2023-04-07 00:48:43 -0400

    move before client init

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,12 @@ import Client from './clients/DingleClient';
 
 dotenv.config();
 
-const client: Client = container.resolve(Client);
-client.init();
-
 process.on('uncaughtException', (e) => {
     console.error(e);
     console.error(e.stack);
     console.log('Process will restart now.');
-    client.destroy();
     process.exit(1);
 });
+
+const client: Client = container.resolve(Client);
+client.init();
