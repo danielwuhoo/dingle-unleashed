3674ff89924c602140410c29a42a5e63d7b3bce4
danielwuhoo <danielwuhoo@gmail.com>
2025-02-07 20:40:02 -0500

    unpin

--- src/operations/PinMessageOperation.ts
+++ src/operations/PinMessageOperation.ts
@@ -10,10 +10,18 @@ export default class PinMessageOperation {
     public async run(): Promise<void> {
         if (!this.interaction.isMessageContextMenuCommand()) return;
 
-        this.interaction.targetMessage.pin();
-        this.interaction.reply({
-            content: 'Message pinned',
-            flags: MessageFlags.Ephemeral,
-        });
+        if (this.interaction.targetMessage.pinned) {
+            this.interaction.targetMessage.unpin();
+            this.interaction.reply({
+                content: 'Message unpinned',
+                flags: MessageFlags.Ephemeral,
+            });
+        } else {
+            this.interaction.targetMessage.pin();
+            this.interaction.reply({
+                content: 'Message pinned',
+                flags: MessageFlags.Ephemeral,
+            });
+        }
     }
 }
